55c855ba2b75c0da2cf8e885219037ba1df63c1c
LitavrinaAA <a.a.litavrina@google.com>
2023-09-01 16:41:22 +0300

    Array of three-digit numbers, count even

---
+++ ex34/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 34: Задайте массив заполненный случайными 
+//положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+int CountEvenNums(int[] array)
+{
+    int length = array.Length;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int arrSize = new Random().Next(4, 10);
+int[] array = new int[arrSize];
+FillArray(array);
+PrintArray(array);
+
+System.Console.WriteLine("Колличество четных чисел: " + CountEvenNums(array));
+
+
